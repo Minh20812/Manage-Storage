2dbb06190acc743d95a0427edde81b1b8a26d9d6
Minh20812 <blossworld12@gmail.com>
2024-09-25 10:01:25 +0700

    prepare for supplier tab

---
+++ frontend/src/components/TableComponent.jsx
@@ -0,0 +1,175 @@
+/** @format */
+
+import { Button, Space, Table, Typography } from "antd";
+import { useEffect, useState } from "react";
+import { Sort } from "iconsax-react";
+import { colors } from "../constants/colors";
+import { Resizable } from "re-resizable";
+import { ModalExportData } from "../modals";
+
+const { Title } = Typography;
+
+const TableComponent = (props) => {
+  const {
+    forms,
+    loading,
+    records,
+    onPageChange,
+    onAddNew,
+    total,
+    scrollHeight,
+    extraColumn,
+    api,
+  } = props;
+
+  const [pageInfo, setPageInfo] =
+    useState <
+    {
+      page,
+      pageSize,
+    } >
+    {
+      page: 1,
+      pageSize: 10,
+    };
+  const [columns, setColumns] = useState([]);
+  const [isVisibleModalExport, setIsVisibleModalExport] = useState(false);
+
+  useEffect(() => {
+    onPageChange(pageInfo);
+  }, [pageInfo]);
+
+  useEffect(() => {
+    if (forms && forms.formItems && forms.formItems.length > 0) {
+      const items = [];
+
+      forms.formItems.forEach((item) =>
+        items.push({
+          key: item.key,
+          dataIndex: item.value,
+          title: item.label,
+          width: item.displayLength,
+        })
+      );
+
+      items.unshift({
+        key: "index",
+        dataIndex: "index",
+        title: "#",
+        align: "center",
+        width: 100,
+      });
+
+      if (extraColumn) {
+        items.push({
+          key: "actions",
+          dataIndex: "",
+          fixed: "right",
+          title: "Action",
+          align: "right",
+          render: (item) => extraColumn(item),
+          width: 100,
+        });
+      }
+
+      setColumns(items);
+    }
+  }, [forms]);
+
+  const RenderTitle = (props) => {
+    const { children, ...restProps } = props;
+    return (
+      <th
+        {...restProps}
+        style={{
+          padding: "6px 12px",
+        }}
+      >
+        <Resizable
+          enable={{ right: true }}
+          onResizeStop={(_e, _direction, _ref, d) => {
+            const item = columns.find(
+              (element) => element.title === children[1]
+            );
+            if (item) {
+              const items = [...columns];
+              const newWidth = item.width + d.width;
+              const index = columns.findIndex(
+                (element) => element.key === item.key
+              );
+
+              if (index !== -1) {
+                items[index].width = newWidth;
+              }
+
+              setColumns(items);
+            }
+          }}
+        >
+          {children}
+        </Resizable>
+      </th>
+    );
+  };
+
+  return (
+    <>
+      <Table
+        pagination={{
+          showSizeChanger: true,
+          onShowSizeChange: (current, size) => {
+            setPageInfo({ ...pageInfo, pageSize: size });
+          },
+          total,
+          onChange(page, pageSize) {
+            setPageInfo({
+              ...pageInfo,
+              page,
+            });
+          },
+          showQuickJumper: true,
+        }}
+        scroll={{
+          y: scrollHeight ? scrollHeight : "calc(100vh - 300px)",
+        }}
+        loading={loading}
+        dataSource={records}
+        columns={columns}
+        bordered
+        title={() => (
+          <div className="row">
+            <div className="col">
+              <Title level={5}>{forms.title}</Title>
+            </div>
+            <div className="col text-right">
+              <Space>
+                <Button type="primary" onClick={onAddNew}>
+                  Add Supplier
+                </Button>
+                <Button icon={<Sort size={20} color={colors.gray600} />}>
+                  Filters
+                </Button>
+                <Button onClick={() => setIsVisibleModalExport(true)}>
+                  Export Excel
+                </Button>
+              </Space>
+            </div>
+          </div>
+        )}
+        components={{
+          header: {
+            cell: RenderTitle,
+          },
+        }}
+      />
+      <ModalExportData
+        visible={isVisibleModalExport}
+        onClose={() => setIsVisibleModalExport(false)}
+        api={api}
+        name={api}
+      />
+    </>
+  );
+};
+
+export default TableComponent;
--- frontend/src/screens/SideScreens/suppliers/MainSuppliers.jsx
+++ frontend/src/screens/SideScreens/suppliers/MainSuppliers.jsx
@@ -1,3 +1,147 @@
+// /** @format */
+
+// import { Edit2, Sort, UserRemove } from "iconsax-react";
+// import { useEffect, useState } from "react";
+// import handleAPI from "../apis/handleAPI";
+// import { Button } from "@headlessui/react";
+// import { ToogleSupplier } from "../../../modals/ToogleSupplier";
+
+// import TableComponent from "../../../components/TableComponent";
+// import { useNavigate, useRoutes, useSearchParams } from "react-router-dom";
+
+// const { Title, Text } = Typography;
+// const { confirm } = Modal;
+
+// const Suppliers = () => {
+//   const [isVisibleModalAddNew, setIsVisibleModalAddNew] = useState(false);
+//   const [suppliers, setSuppliers] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [supplierSelected, setSupplierSelected] = useState();
+//   const [page, setPage] = useState(1);
+//   const [pageSize, setPageSize] = useState(10);
+//   const [total, setTotal] = useState(10);
+//   const [forms, setForms] = useState();
+
+//   useEffect(() => {
+//     getData();
+//   }, []);
+
+//   useEffect(() => {
+//     getSuppliers();
+//   }, [page, pageSize]);
+
+//   const getData = async () => {
+//     setIsLoading(true);
+//     try {
+//       await getFroms();
+//     } catch (error) {
+//       message.error(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const getFroms = async () => {
+//     const api = `/supplier/get-form`;
+//     const res = await handleAPI(api);
+
+//     res.data && setForms(res.data);
+//   };
+
+//   const getSuppliers = async () => {
+//     const api = `/supplier?page=${page}&pageSize=${pageSize}`;
+//     setIsLoading(true);
+//     try {
+//       const res = await handleAPI(api);
+//       res.data && setSuppliers(res.data.items);
+
+//       const items = [];
+
+//       res.data.items.forEach((item, index) =>
+//         items.push({
+//           index: (page - 1) * pageSize + (index + 1),
+//           ...item,
+//         })
+//       );
+
+//       setSuppliers(items);
+//       setTotal(res.data.total);
+//     } catch (error) {
+//       message.error(error.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   const removeSuppiler = async (id) => {
+//     try {
+//       await handleAPI(`/supplier/remove?id=${id}`, undefined, "delete");
+
+//       await getSuppliers();
+//       message.error("Remove supplier successfully!!!");
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   return forms ? (
+//     <div>
+//       <TableComponent
+//         api="supplier"
+//         onPageChange={(val) => {
+//           setPage(val.page);
+//           setPageSize(val.pageSize);
+//         }}
+//         onAddNew={() => {
+//           setIsVisibleModalAddNew(true);
+//         }}
+//         loading={isLoading}
+//         forms={forms}
+//         records={suppliers}
+//         total={total}
+//         extraColumn={(item) => (
+//           <div>
+//             <Button
+//               type="text"
+//               onClick={() => {
+//                 setSupplierSelected(item);
+//                 setIsVisibleModalAddNew(true);
+//               }}
+//               icon={<Edit2 size={18} className="text-info" />}
+//             />
+
+//             <Button
+//               onClick={() =>
+//                 confirm({
+//                   title: "Comfirm",
+//                   content: "Are you sure you want to remove this supplier?",
+//                   onOk: () => removeSuppiler(item._id),
+//                 })
+//               }
+//               type="text"
+//               icon={<UserRemove size={18} className="text-danger" />}
+//             />
+//           </div>
+//         )}
+//       />
+
+//       <ToogleSupplier
+//         visible={isVisibleModalAddNew}
+//         onClose={() => {
+//           supplierSelected && getSuppliers();
+//           setSupplierSelected(undefined);
+//           setIsVisibleModalAddNew(false);
+//         }}
+//         onAddNew={(val) => setSuppliers([...suppliers, val])}
+//         supplier={supplierSelected}
+//       />
+//     </div>
+//   ) : (
+//     <Empty />
+//   );
+// };
+
+// export default Suppliers;
 import React from "react";
 
 const MainSuppliers = () => {
