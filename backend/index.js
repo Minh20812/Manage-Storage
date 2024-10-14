ce3a9aff180bef0ef02f2dc51d9bb7ef39ad4eb1
Minh20812 <blossworld12@gmail.com>
2024-10-14 10:49:27 +0700

    change

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,18 @@ connectDB(); //kết nối đến cơ sở dữ liệu
 
 const app = express(); //khởi tạo express app
 
+app.use(
+  cors({
+    origin: [
+      "https://manage-storage.web.app",
+      "https://client-manage-storage.vercel.app/",
+      "http://localhost:3000",
+    ], // thêm tất cả các origin hợp lệ
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true, // Nếu bạn sử dụng cookie để xác thực
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
